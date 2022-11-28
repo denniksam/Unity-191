ea934c1e1a307f2659c686b4ebee7377e157e41c
Denys Samoilenko <denniksam@gmail.com>
2022-11-28 12:07:04 +0200

    2022-11-28--1

---
+++ Intro3D/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Sphere;      // ссылка на шарик
+    private Vector3 cam_sphere;     // вектор камера-шарик
+    private float mouseX;           // накопитель сдвигов мыши по Х
+    private float mouseY;           // по Y
+    private float zoom;             // приближение камеры
+
+    private const float MAX_X_ANGLE = 70;        // предельные углы поворота
+    private const float MIN_X_ANGLE = 35;        // камеры вокруг оси Х
+    private const float SENSITIVITY_X = 2;       // чувствительность к мыши
+    private const float SENSITIVITY_Y = 3;       // чувствительность к мыши
+    private const float MAX_ZOOM = 2;            // предельные значения приближения
+    private const float MIN_ZOOM = 0.2f;         // камеры к шарику
+    private const float SENSITIVITY_ZOOM = 10;   // чувствительность к колесу мыши
+
+
+    void Start()
+    {
+        cam_sphere = this.transform.position - Sphere.transform.position;
+        mouseY = this.transform.eulerAngles.x;
+        zoom = 1;
+    }
+
+    private void Update()
+    {
+        // накопление данных о движении мыши
+        mouseY -= SENSITIVITY_X * Input.GetAxis("Mouse Y");
+        if (mouseY < MIN_X_ANGLE) mouseY = MIN_X_ANGLE;
+        if (mouseY > MAX_X_ANGLE) mouseY = MAX_X_ANGLE;
+
+        mouseX += SENSITIVITY_Y * Input.GetAxis("Mouse X");
+
+        // поворот колеса мыши (scrolling)
+        if (Input.mouseScrollDelta != Vector2.zero)
+        {
+            zoom -= Input.mouseScrollDelta.y / SENSITIVITY_ZOOM;
+            if (zoom < MIN_ZOOM) zoom = MIN_ZOOM;
+            if (zoom > MAX_ZOOM) zoom = MAX_ZOOM;
+        }
+    }
+
+    void LateUpdate()
+    {
+        // пересчитываем позицию камеры относительно шарика
+        this.transform.position = Sphere.transform.position 
+            + Quaternion.Euler(0, mouseX, 0) * cam_sphere * zoom ;
+
+        // поворачиваем камеру
+        this.transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
+    }
+}
+// ограничить угол поворота камеры вокруг Х в диапазоне 35..70 градусов
---
+++ Intro3D/Assets/Scripts/Sphere.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField]
+    private Camera cam;
+    private Vector3 forceDirection;
+
+    private float FORCE_MAGNITUDE = 2;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(rb.velocity.y == 0) rb.AddForce(Vector3.up * 100);
+        }
+        float dx = Input.GetAxis("Horizontal");
+        float dy = Input.GetAxis("Vertical");       // стрелки "вверх,вниз" упраляют по Z
+        // rb.AddForce(new Vector3(dx, 0, dy) * FORCE_MAGNITUDE);    // в векторе силы Y -> Z
+        // корректируем на поворот камеры: вперед - это куда смотрит камера
+        // rb.AddForce(cam.transform.forward * dy * FORCE_MAGNITUDE);
+        forceDirection = cam.transform.forward;      // forward камеры наклонен вниз, усилия вдавливают/поднимают шарик
+        forceDirection.y = 0;                        // убираем вертикальную составляющую вектора, но тогда его
+        forceDirection = forceDirection.normalized   // длина уменьшается. normalized - сохраняет направление и задает длину 1
+            * dy * FORCE_MAGNITUDE;                  // умножаем на ввод пользователя
+        forceDirection += cam.transform.right        // right камеры не наклонен по Y, поэтому 
+            * dx * FORCE_MAGNITUDE;                  // коррекции не нужны
+        rb.AddForce(forceDirection);                 // 
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        GameObject.Destroy(other.gameObject);
+    }
+}
+/* Д.З. Реализовать отсчет позиции камеры не от центра шарика, а от его верхней
+ * точки (чуть выше поверхности). 
+ * ** Расширить пределы zoom в область нуля (ноль - от первого лица, камера ровно над персонажем)
+ *    (сложность - выход из нуля в направлении камеры)
+ */
